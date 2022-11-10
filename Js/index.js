bca1c8e631bb799cb1421e04aa31ec4e77a4b6b5
Fernanda Miranda <fermfriass@gmail.com>
2022-11-10 00:46:11 -0600

    PreEntrega3Miranda

---
+++ Js/index.js
@@ -0,0 +1,6 @@
+const navToggle = document.querySelector("toggle");
+const navMenu =  ducument.querySelector("barranavegacion");
+
+navToggle.addEventListener("click", (e) => {
+    barranavegacion.classList.toggle("barranavegacion_visible");
+});
